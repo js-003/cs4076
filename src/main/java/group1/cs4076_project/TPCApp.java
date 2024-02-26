89703b19bc0f0fd16c82d39f1771f27ca0e9716b
js-003 <jakubsokal13@gmail.com>
2024-02-26 00:26:27 +0000

    Created main stage, add, remove , display buttons, updated add

--- src/main/java/group1/cs4076_project/TPCApp.java
+++ src/main/java/group1/cs4076_project/TPCApp.java
@@ -6,47 +6,177 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Cursor;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
-
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class TPCApp extends Application {
+    private ArrayList<String> List = new ArrayList<>();
+    private HashMap<String,String> hashMap = new HashMap<>();
     private TextField textbox;
-    Label l = new Label("Response From Server Will Display Here");
 
     @Override
     public void start(Stage stage) throws IOException {
         GridPane G = new GridPane();
-        Tab tabAdd = new Tab("Add Class");
-        Tab tabRemove = new Tab("Remove Class");
-        Tab tabDisplay = new Tab("Display Schedule");
-        TabPane tabs = new TabPane();
-        tabs.getTabs().add(tabAdd);
-        tabs.getTabs().add(tabRemove);
-        tabs.getTabs().add(tabDisplay);
-        G.add(tabs,0,0);
-        Scene scene = new Scene(G, 480, 300);
-        stage.setTitle("Texting App");
+        G.setAlignment(Pos.CENTER);
+        G.add(buttonsMain(),0,1);
+        stage.setResizable(false);
+        stage.setTitle("Class Scheduler");
+        Scene scene = new Scene(G,640,480);
         stage.setScene(scene);
         stage.show();
         try {
+            System.out.println("Starting Connection...");
             host = InetAddress.getLocalHost();
         } catch (Exception e) {
-            System.out.print("NO");
+            System.out.println("NO");
         }
-        System.out.print("Starting Connectiopn");
+        System.out.println("Connected");
        // startConnection();
+    }
+
+
+    public HBox buttonsMain(){
+        HBox hBox = new HBox();
+        Button add = new Button("Add Class");
+        Button rem = new Button("Remove Class");
+        Button dis = new Button("Display Class Schedule");
+        Insets n = new Insets(15);
+        add.setPadding(n);
+        add.setPrefWidth(150);
+        rem.setPrefWidth(150);
+        rem.setPadding(n);
+        dis.setPadding(n);
+        hBox.getChildren().add(add);
+        hBox.getChildren().add(rem);
+        hBox.getChildren().add(dis);
+        add.setOnAction(actionEvent ->  addClass());
+        hBox.setSpacing(40);
+        return hBox;
+    }
+    DatePicker dp = new DatePicker();
+    TextField text = new TextField();
+    TextField cText = new TextField();
+    ChoiceBox<String> cb = new ChoiceBox<>();
+    public void addClass(){
+        VBox vBox = new VBox();
+        vBox.setSpacing(25);
+        Stage s = new Stage();
+        GridPane grid = new GridPane();
+        dp.setPromptText("Select Date:");
+        dp.setMaxWidth(245);
+        text.setMaxWidth(245);
+        cText.setMaxWidth(245);
+        text.setPromptText("Enter A Room Number: ");
+        cText.setPromptText("Enter A Class Name And Year: LM051-2022");
+        dp.setEditable(false);
+        cb.setValue("Class Time");
+        cb.setMaxWidth(245);
+        for(int i = 9 ; i<18; i++){
+            cb.getItems().add(i+":00");
+        }
+        vBox.setPrefWidth(640);
+        vBox.getChildren().add(m());
+        HBox hBox = new HBox();
+        grid.setAlignment(Pos.TOP_CENTER);
+        hBox.setAlignment(Pos.CENTER);
+        vBox.setAlignment(Pos.CENTER);
+        hBox.setSpacing(25);
+        Button show = new Button("Save");
+        Button Stop = new Button("Stop");
+        show.setPadding(new Insets(10));
+        Stop.setPadding(new Insets(10));
+        hBox.getChildren().addAll(show,Stop);
+        vBox.getChildren().addAll(cb,dp,text,cText,hBox);
+        show.setOnAction(actionEvent -> {
+            String x = "";
+            Alert a1 = new Alert(Alert.AlertType.ERROR);
+                if (cText.getText().isEmpty()) {
+                    a1.setContentText("Please enter a class name!");
+                    a1.show();
+                } else if (text.getText().isEmpty()) {
+                    a1.setContentText("Please enter a room number!");
+                    a1.show();
+                } else if (dp.getValue().equals(null)){
+                    a1.setContentText("Please select a date");
+                    a1.show();
+                } else if(cb.getValue().equals("Class Time")){
+                    a1.setContentText("Please select a class time");
+                    a1.show();
+                }
+                    x += cText.getText() + " " + text.getText() + " " + dp.getValue() + " " + cb.getValue();
+                    List.add(x);
+            String g = cText.toString();
+            hashMap.put(cText.toString(),x);
+            for(int i = 0; i<hashMap.size();i++){
+                        if(g.contains(cText.toString())){
+                            g = hashMap.get(cText.toString());
+                            g += text.getText() + " " + dp.getValue() + " " + cb.getValue();
+                            hashMap.put(cText.toString(),g);
+                            System.out.println(hashMap.toString());
+                        }
+
+                    //System.out.println(List);
+                }
+        });
+        Stop.setOnAction(actionEvent -> Platform.exit());
+        grid.add(vBox,0,0);
+        s.setTitle("Add Class To Schedule");
+        Scene scene = new Scene(grid,640,480);
+        s.setScene(scene);
+        s.show();
+    }
+
+
+    public MenuBar m(){
+        MenuBar mb = new MenuBar();
+        Menu options = new Menu("Options");
+        MenuItem clear = new MenuItem("Clear");
+        options.getItems().add(clear);
+
+        clear.setOnAction(actionevent -> {
+            text.clear();
+            cText.clear();
+            dp.setValue(null);
+            cb.setValue("Class Time");
+        });
+        mb.getMenus().add(options);
+        return mb;
+    }
+    private VBox DisplayTab(){
+        VBox vBox = new VBox();
+        HBox hBox = new HBox();
+        vBox.setSpacing(40);
+        hBox.setSpacing(20);
+        hBox.setAlignment(Pos.CENTER);
+        vBox.setAlignment(Pos.CENTER);
+        Button show = new Button("Show");
+        Button Stop = new Button("Stop");
+
+        hBox.getChildren().add(show);
+        hBox.getChildren().add(Stop);
+        show.setOnAction(actionEvent -> System.out.println("HELLO"));
+        Stop.setOnAction(actionEvent -> Platform.exit());
+        TextField textField = new TextField();
+        textField.setPromptText("Enter Class Name And Year: LM051-2022");
+        textField.setMaxWidth(230);
+        vBox.getChildren().add(m());
+        vBox.getChildren().add(textField);
+        vBox.getChildren().add(hBox);
+        return vBox;
     }
 
     public TextField text(String text) {
