8b0ef392630554353004c8d59ae290b5f3e34d57
js-003 <jakubsokal13@gmail.com>
2024-02-27 23:33:42 +0000

    Updated add, added CSV file and storing class times

--- src/main/java/group1/cs4076_project/TPCApp.java
+++ src/main/java/group1/cs4076_project/TPCApp.java
@@ -2,39 +2,49 @@ package group1.cs4076_project;
 
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Cursor;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.image.Image;
 import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 
 public class TPCApp extends Application {
     private ArrayList<String> List = new ArrayList<>();
     private HashMap<String,String> hashMap = new HashMap<>();
-    private TextField textbox;
+    public TreeMap<String,String[]> db = new TreeMap<>();
 
     @Override
     public void start(Stage stage) throws IOException {
         GridPane G = new GridPane();
         G.setAlignment(Pos.CENTER);
         G.add(buttonsMain(),0,1);
+        VBox vBox = new VBox();
+        HBox h = new HBox();
+        Button stop = new Button("Stop");
+        stop.setPrefWidth(180);
+        stop.setPrefHeight(50);
+        Text m = new Text("Select One Of The Buttons Below");
+        m.setFont(new Font(20));
+        m.setTextAlignment(TextAlignment.CENTER);
+        h.setAlignment(Pos.CENTER);
+        h.getChildren().add(m);
+        G.add(h,0,0);
+        vBox.getChildren().add(stop);
+        vBox.setAlignment(Pos.CENTER);
+        G.setVgap(40);
+        G.add(vBox,0,2);
         stage.setResizable(false);
         stage.setTitle("Class Scheduler");
+        stage.getIcons().add(new Image("file:C:/Users/jakub/OneDrive/Pulpit/Studia/2/cs4076_project/src/main/java/group1/icon.png"));
         Scene scene = new Scene(G,640,480);
         stage.setScene(scene);
         stage.show();
@@ -45,15 +55,18 @@ public class TPCApp extends Application {
             System.out.println("NO");
         }
         System.out.println("Connected");
+
        // startConnection();
     }
 
 
     public HBox buttonsMain(){
         HBox hBox = new HBox();
+
         Button add = new Button("Add Class");
         Button rem = new Button("Remove Class");
         Button dis = new Button("Display Class Schedule");
+
         Insets n = new Insets(15);
         add.setPadding(n);
         add.setPrefWidth(150);
@@ -63,15 +76,24 @@ public class TPCApp extends Application {
         hBox.getChildren().add(add);
         hBox.getChildren().add(rem);
         hBox.getChildren().add(dis);
-        add.setOnAction(actionEvent ->  addClass());
+        add.setOnAction(actionEvent -> {
+            try {
+                addClass();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
         hBox.setSpacing(40);
         return hBox;
     }
+
+
     DatePicker dp = new DatePicker();
     TextField text = new TextField();
     TextField cText = new TextField();
     ChoiceBox<String> cb = new ChoiceBox<>();
-    public void addClass(){
+
+    public void addClass() throws IOException {
         VBox vBox = new VBox();
         vBox.setSpacing(25);
         Stage s = new Stage();
@@ -103,8 +125,8 @@ public class TPCApp extends Application {
         vBox.getChildren().addAll(cb,dp,text,cText,hBox);
         show.setOnAction(actionEvent -> {
             String x = "";
-            Alert a1 = new Alert(Alert.AlertType.ERROR);
-                if (cText.getText().isEmpty()) {
+            Alert a1 = new Alert(Alert.AlertType.WARNING);
+            if (cText.getText().isEmpty()) {
                     a1.setContentText("Please enter a class name!");
                     a1.show();
                 } else if (text.getText().isEmpty()) {
@@ -117,36 +139,57 @@ public class TPCApp extends Application {
                     a1.setContentText("Please select a class time");
                     a1.show();
                 }
-                    x += cText.getText() + " " + text.getText() + " " + dp.getValue() + " " + cb.getValue();
-                    List.add(x);
-            String g = cText.toString();
-            hashMap.put(cText.toString(),x);
-            for(int i = 0; i<hashMap.size();i++){
-                        if(g.contains(cText.toString())){
-                            g = hashMap.get(cText.toString());
-                            g += text.getText() + " " + dp.getValue() + " " + cb.getValue();
-                            hashMap.put(cText.toString(),g);
-                            System.out.println(hashMap.toString());
-                        }
+                x += text.getText() + " " + dp.getValue() + " " + cb.getValue();
+                if(hashMap.toString().contains(cText.getText())){
+                    if(hashMap.get(cText.getText()).contains(dp.getValue().toString()) && hashMap.get(cText.getText()).contains(cb.getValue()) ) {
+                        a1.setContentText("The time selected is already taken on this date "+dp.getValue().toString()); a1.show();
+                    }else hashMap.put(cText.getText(), hashMap.get(cText.getText()) + "," + x);
+            }else hashMap.put(cText.getText(),x);
 
-                    //System.out.println(List);
-                }
+                String[] da = hashMap.toString().split(",")   ;
+                System.out.println(Arrays.toString(da));
+            List = new ArrayList<>();
+            List.add(Arrays.toString(da).replace("[","").replace("]",""));
+            try{
+                CSV();
+            }catch (Exception e){
+                System.out.println("ERROR");
+            }
         });
-        Stop.setOnAction(actionEvent -> Platform.exit());
+        Stop.setOnAction(actionEvent -> s.close());
         grid.add(vBox,0,0);
         s.setTitle("Add Class To Schedule");
+        s.getIcons().add(new Image("file:C:/Users/jakub/OneDrive/Pulpit/Studia/2/cs4076_project/src/main/java/group1/icon.png"));
         Scene scene = new Scene(grid,640,480);
         s.setScene(scene);
         s.show();
     }
-
-
+public void CsvReader() throws FileNotFoundException {
+    //parsing a CSV file into Scanner class constructor
+    Scanner sc = new Scanner(file);
+    sc.useDelimiter(",");   //sets the delimiter pattern
+    while (sc.hasNext())  //returns a boolean value
+    {
+        List.add(sc.next());  //find and returns the next complete token from this scanner
+    }
+    sc.close();
+}
+    File file = new File("C:/Users/jakub/OneDrive/Pulpit/Studia/2/cs4076_project/src/main/resources/group1/cs4076_project/data.csv");
+    public void CSV() throws FileNotFoundException{
+        CsvReader();
+        PrintWriter print = new PrintWriter(file);
+        for(int i = 0; i<List.size();i++){
+            print.print(List.get(i));
+        }
+        print.close();
+    }
     public MenuBar m(){
         MenuBar mb = new MenuBar();
         Menu options = new Menu("Options");
         MenuItem clear = new MenuItem("Clear");
+        MenuItem checkCon = new MenuItem("Check Connection");
+        options.getItems().add(checkCon);
         options.getItems().add(clear);
-
         clear.setOnAction(actionevent -> {
             text.clear();
             cText.clear();
@@ -156,7 +199,7 @@ public class TPCApp extends Application {
         mb.getMenus().add(options);
         return mb;
     }
-    private VBox DisplayTab(){
+    private VBox DisplayTab() {
         VBox vBox = new VBox();
         HBox hBox = new HBox();
         vBox.setSpacing(40);
@@ -165,7 +208,6 @@ public class TPCApp extends Application {
         vBox.setAlignment(Pos.CENTER);
         Button show = new Button("Show");
         Button Stop = new Button("Stop");
-
         hBox.getChildren().add(show);
         hBox.getChildren().add(Stop);
         show.setOnAction(actionEvent -> System.out.println("HELLO"));
@@ -178,15 +220,6 @@ public class TPCApp extends Application {
         vBox.getChildren().add(hBox);
         return vBox;
     }
-
-    public TextField text(String text) {
-        textbox = new TextField();
-        textbox.setPromptText(text);
-        textbox.setFocusTraversable(false);
-        textbox.setPrefWidth(50);
-        return textbox;
-    }
-
 
     public Socket clientSocket;
     private PrintWriter out;
@@ -222,4 +255,3 @@ public class TPCApp extends Application {
             launch();
     }
 }
-
